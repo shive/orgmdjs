710d5aa1eb5ea0ab8556fb011392e765f7676f07
hshibuya <goe@fuzz.co.jp>
2014-04-03 03:42:50 +0900

    表

--- html/js/render.js
+++ html/js/render.js
@@ -44,7 +44,10 @@ function onload() {
                 s = s.replace(/^\*\*\*\* /, '##### ');
                 s = s.replace(/(^|[^`])\{#([\w\d_\-@$]+)\}/, '$1<a href="#$2" name="$2" style="font-size:xx-small; vertical-align:top;">†</a>');
                 s = s.replace(/(^|[^`])(\[(20\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d)\])/, '$1<a href="#$3$4$5-$6$7" name="$3$4$5-$6$7" style="font-size:xx-small; vertical-align:top;"><code>$2</code></a>')
-                s = s.replace(/\[\[([^\]]+)\]\[([^\]]+)\]\]/,'[$2]($1)'); // ハイパーリンク
+                s = s.replace(/(^|[^`])\[\[([^\]]+)\]\[([^\]]+)\]\]/,'$1[$3]($2)'); // ハイパーリンク
+                if(s[0] == '|'){
+                    s = s.replace(/-\+-/g, '-|-');
+                }
                 return s;
             });
         };
